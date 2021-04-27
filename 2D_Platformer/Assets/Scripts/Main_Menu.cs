f5ae5623b9d4cd4e69d7493e35d892f1054de338
Dmitry-Poltaradnev <poltaradnev@gmail.com>
2021-04-27 19:47:19 +0300

    Added ui menu and ui script.

---
+++ 2D_Platformer/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;//Библиотека для работы со сценами.
+
+public class Main_Menu : MonoBehaviour
+{
+    public void StartHandler()//Start button.
+    {
+        SceneManager.LoadScene(1);//Загрузка сцены по названию(также можно добавить по индексу), также необходимо добавить в Build Setting данную сцену.
+    }
+
+    public void ExitHandler()//Exit button.
+    {
+        Application.Quit();//Метод отвечает за выход.
+    }
+}
